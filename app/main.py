7e7ffa3316163d11df555427426f212e5cc73f72
Zhora1142 <avdonin.egor@gmail.com>
2021-08-30 18:36:47 +0300

    Удаление user_id из тиктока

--- app/main.py
+++ app/main.py
@@ -133,7 +133,7 @@ def formatting(u):
 def tiktok(u):
     msg_id = bot.send_message(u.chat_id, 'Загрузка видеозаписи...')['message_id']
     try:
-        result = requests.post(url=url + 'download_video', data={'url': u.url, 'chat_id': u.chat_id}).json()
+        result = requests.post(url=url + 'download_video', data={'url': u.url}).json()
     except Exception as e:
         logging.error('TikTok downloading error: ' + str(e))
         bot.edit_message(u.chat_id, msg_id, 'Не удалось загрузить видео.')
@@ -182,7 +182,8 @@ if __name__ == '__main__':
 
             elif update.type == 'url':
                 if 'tiktok' in update.url:
-                    tiktok(update)
+                    t = Thread(target=tiktok, args=(update,))
+                    t.start()
                 else:
                     t = Thread(target=formatting, args=(update,))
                     t.start()
